b7b380c6ec35f3c683bdde1de9bc406a9c029a07
fruchti <fruchti@gvfr.de>
2017-02-24 14:27:27 +0100

    Add example plot

--- demo/sd/inc/fatfs-stm32/.local.vimrc
+++
@@ -1,3 +0,0 @@
-nnoremap <leader>m :wa <BAR> :Make -C ../..<CR>
-nnoremap <leader>k :wa <BAR> :Make -C ../.. program<CR>
-set errorformat=SHIT
--- demo/sd/plot.py
+++ demo/sd/plot.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 
 data = np.genfromtxt('example-log.csv', delimiter = ',', skip_header = 4, names = ['t', 'T'])
 
-plt.figure(figsize = (500 / 96, 200 / 96))
 plt.plot(data['t'] / 1e3, data['T'])
 plt.ylabel('T/°C')
 plt.xlabel('t/s')
-#plt.show()
-plt.savefig("plt.png")
+plt.show()
